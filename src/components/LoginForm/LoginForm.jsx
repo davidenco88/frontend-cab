b715c3d31a5bbdbe6d94698484492117c352be72
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2023-06-21 19:36:15 +0200

    refactor: :sparkles: add required in all inputs and check default

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -11,21 +11,20 @@ function LoginForm() {
           <div className="sign-in-htm">
             <div className="group">
               <label htmlFor="pass" className="label">Email Address</label>
-              <input id="pass" type="text" className="input" />
+              <input id="pass" type="text" className="input" required />
             </div>
             <div className="group">
               <label htmlFor="pass" className="label">Password</label>
-              <input id="pass" type="password" className="input" data-type="password" />
+              <input id="pass" type="password" className="input" data-type="password" required />
             </div>
             <div className="group">
               <h4 className='rol-header'>Would you like to sign in as?</h4>
-
               <div className='user-rol'>
                 <label htmlFor="check-client">
-                  <input id='check-client' type="radio" name='user-rol' /> CLIENT
+                  <input id='check-client' type="radio" name='singin-rol' checked /> CLIENT
                 </label>
                 <label htmlFor="check-driver">
-                  <input id='check-driver' type="radio" name='user-rol' /> DRIVER
+                  <input id='check-driver' type="radio" name='singin-rol' /> DRIVER
                 </label>
               </div>
             </div>
@@ -40,23 +39,34 @@ function LoginForm() {
           <div className="sign-up-htm">
             <div className="group">
               <label htmlFor="first-name" className="label">First Name</label>
-              <input id="first-name" type="text" className="input" />
+              <input id="first-name" type="text" className="input" required />
             </div>
             <div className="group">
               <label htmlFor="last-name" className="label">Last Name</label>
-              <input id="last-name" type="text" className="input" />
+              <input id="last-name" type="text" className="input" required />
             </div>
             <div className="group">
               <label htmlFor="pass" className="label">Email Address</label>
-              <input id="pass" type="text" className="input" />
+              <input id="pass" type="text" className="input" required />
             </div>
             <div className="group">
               <label htmlFor="pass" className="label">Password</label>
-              <input id="pass" type="password" className="input" data-type="password" />
+              <input id="pass" type="password" className="input" data-type="password" required />
             </div>
             <div className="group">
               <label htmlFor="pass" className="label">Repeat Password</label>
-              <input id="pass" type="password" className="input" data-type="password" />
+              <input id="pass" type="password" className="input" data-type="password" required />
+            </div>
+            <div className="group">
+              <h4 className='rol-header'>Would you like to sign up as?</h4>
+              <div className='user-rol'>
+                <label htmlFor="check-client">
+                  <input id='check-client' type="radio" name='user-rol' checked /> CLIENT
+                </label>
+                <label htmlFor="check-driver">
+                  <input id='check-driver' type="radio" name='user-rol' /> DRIVER
+                </label>
+              </div>
             </div>
             <div className="group">
               <input type="submit" className="button" value="Sign Up" />
