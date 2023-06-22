33fbc0a3926f1067930ff0d9839698bbd799971b
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-06-22 03:47:30 -0500

    Feature/login fetch (#11)
    * fix: :bug: repeat-ids-checkedInputs
    
    * feat: :sparkles: login-working
    
    * refactor: :adhesive_bandage: add-error-handling
    
    Dejo unos comentarios para David o tal vez yo del futuro en el LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,30 +1,93 @@
-import './LoginForm.css';
+import { login } from "../../services/auth";
+import "./LoginForm.css";
 
 function LoginForm() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const signInData = Object.fromEntries(formData);
+
+    const loginPayload = await login(signInData);
+    console.log(
+      "🚀 ~ file: LoginForm.jsx:12 ~ handleSubmit ~ loginPayload:",
+      loginPayload
+    );
+
+    //como la propiedad status solo existe cuando falla entonces solo salta
+    //cuando hay error
+    if (loginPayload.status) {
+      console.log("Algo anda mal");
+      //ACA PODRIA IMPLEMENTARSE UN USESTATE CON UN BOOLEANO
+      //CUANDO SEA TRUE ENTONCES APARECE UN COMPONENTE QUE DICE EL
+      //MENSAJE ENVIADO DESDE EL BACKEND CON EL ERROR
+      //ASI FUNCIONARIA CON DIFERENTES MENSAJES Y VARIAS PAGS
+    }
+  }
+
   return (
     <div className="c-loginForm">
       <div className="loginForm">
-        <input id="tab-1" type="radio" name="tab" className="sign-in" checked />
-        <label htmlFor="tab-1" className="tab">Sign In</label>
-        <input id="tab-2" type="radio" name="tab" className="sign-up" /><label htmlFor="tab-2" className="tab">Sign Up</label>
+        <input
+          id="tab-1"
+          type="radio"
+          name="tab"
+          className="sign-in"
+          defaultChecked
+        />
+        <label htmlFor="tab-1" className="tab">
+          Sign In
+        </label>
+        <input id="tab-2" type="radio" name="tab" className="sign-up" />
+        <label htmlFor="tab-2" className="tab">
+          Sign Up
+        </label>
         <div className="login-form">
-          <div className="sign-in-htm">
+          <form className="sign-in-htm" onSubmit={handleSubmit}>
             <div className="group">
-              <label htmlFor="pass" className="label">Email Address</label>
-              <input id="pass" type="text" className="input" required />
+              <label htmlFor="email" className="label">
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="input"
+                required
+              />
             </div>
             <div className="group">
-              <label htmlFor="pass" className="label">Password</label>
-              <input id="pass" type="password" className="input" data-type="password" required />
+              <label htmlFor="password" className="label">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                className="input"
+                data-type="password"
+                required
+              />
             </div>
             <div className="group">
-              <h4 className='rol-header'>Would you like to sign in as?</h4>
-              <div className='user-rol'>
-                <label htmlFor="check-client">
-                  <input id='check-client' type="radio" name='singin-rol' checked /> CLIENT
+              <h4 className="rol-header">Would you like to sign in as?</h4>
+              <div className="user-rol">
+                <label htmlFor="check-client-signIn">
+                  <input
+                    id="check-client-signIn"
+                    type="radio"
+                    name="singin-rol"
+                    defaultChecked
+                  />{" "}
+                  CLIENT
                 </label>
-                <label htmlFor="check-driver">
-                  <input id='check-driver' type="radio" name='singin-rol' /> DRIVER
+                <label htmlFor="check-driver-signIn">
+                  <input
+                    id="check-driver-signIn"
+                    type="radio"
+                    name="singin-rol"
+                  />{" "}
+                  DRIVER
                 </label>
               </div>
             </div>
@@ -35,43 +98,73 @@ function LoginForm() {
             <div className="foot-lnk">
               <a href="#">Olvide mi contraseña</a>
             </div>
-          </div>
-          <div className="sign-up-htm">
+          </form>
+
+          <form className="sign-up-htm">
             <div className="group">
-              <label htmlFor="first-name" className="label">First Name</label>
+              <label htmlFor="first-name" className="label">
+                First Name
+              </label>
               <input id="first-name" type="text" className="input" required />
             </div>
             <div className="group">
-              <label htmlFor="last-name" className="label">Last Name</label>
+              <label htmlFor="last-name" className="label">
+                Last Name
+              </label>
               <input id="last-name" type="text" className="input" required />
             </div>
             <div className="group">
-              <label htmlFor="pass" className="label">Email Address</label>
-              <input id="pass" type="text" className="input" required />
+              <label htmlFor="email-signUp" className="label">
+                Email Address
+              </label>
+              <input id="email-signUp" type="text" className="input" required />
             </div>
             <div className="group">
-              <label htmlFor="pass" className="label">Password</label>
-              <input id="pass" type="password" className="input" data-type="password" required />
+              <label htmlFor="password-signUp" className="label">
+                Password
+              </label>
+              <input
+                id="password-signUp"
+                type="password"
+                className="input"
+                data-type="password"
+                required
+              />
             </div>
             <div className="group">
-              <label htmlFor="pass" className="label">Repeat Password</label>
-              <input id="pass" type="password" className="input" data-type="password" required />
+              <label htmlFor="password-signUp-rep" className="label">
+                Repeat Password
+              </label>
+              <input
+                id="password-signUp-rep"
+                type="password"
+                className="input"
+                data-type="password"
+                required
+              />
             </div>
             <div className="group">
-              <h4 className='rol-header'>Would you like to sign up as?</h4>
-              <div className='user-rol'>
+              <h4 className="rol-header">Would you like to sign up as?</h4>
+              <div className="user-rol">
                 <label htmlFor="check-client">
-                  <input id='check-client' type="radio" name='user-rol' checked /> CLIENT
+                  <input
+                    id="check-client"
+                    type="radio"
+                    name="user-rol"
+                    defaultChecked
+                  />{" "}
+                  CLIENT
                 </label>
                 <label htmlFor="check-driver">
-                  <input id='check-driver' type="radio" name='user-rol' /> DRIVER
+                  <input id="check-driver" type="radio" name="user-rol" />{" "}
+                  DRIVER
                 </label>
               </div>
             </div>
             <div className="group">
               <input type="submit" className="button" value="Sign Up" />
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
---
+++ src/services/auth.js
@@ -0,0 +1,23 @@
+const BASE_URL = "http://localhost:8080";
+
+export async function login(data) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(`${BASE_URL}/auth/local/login`, options);
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    const errorData = await response.json();
+    return {
+      status: response.status,
+      message: errorData.message,
+    };
+  }
+}
