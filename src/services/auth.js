79828c337dc55b4daa13d37e177d08dc379d7070
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-06-27 15:22:18 -0500

    Feature/context use reducer (#18)
    * feat: :sparkles: add-useReducer-useContextHooks
    
    * feat: :sparkles: fill-initialState-use-envRoute

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8080";
 
 export async function login(data) {
   const options = {
