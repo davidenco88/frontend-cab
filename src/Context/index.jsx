4c6276974690f3739afdd5781ac6b7182db48ed6
Federico <federicoc03@gmail.com>
2023-06-27 13:13:45 -0500

    feat: :sparkles: add-useReducer-useContextHooks

---
+++ src/Context/index.jsx
@@ -0,0 +1,36 @@
+import { createContext, useReducer, useContext } from 'react';
+import { reducer } from './reducer'
+
+const AppContext = createContext();
+
+export function ContextProvider({ children }) {
+
+  const initialState = {}
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const store = { state, dispatch }
+
+  return (
+    <AppContext.Provider value={store}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export const useSelector = () => {
+  const context = useContext(AppContext)
+
+  if (!context) {
+    throw new Error(`Cannot use outside contextProvider`);
+  }
+  return context.state;
+}
+
+export const useDispatch = () => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error(`Cannot use outside contextProvider`);
+  }
+  return context.dispatch;
+};
