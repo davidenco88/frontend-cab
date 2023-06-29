dcfdbcb82a73aeb7479ef021c896155de8620f7e
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2023-06-29 19:22:19 +0200

    refactor: :recycle: refactor to mock cars in home (#24)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,18 @@
-import { useState, useEffect } from 'react';
-import { fetchCars } from '../services/cars';
 import HeroHome from '../components/HeroHome/HeroHome';
 import FleetSlider from '../components/FleetSlider/FleetSlider';
 import HowToGet from '../components/HowToGet/HowToGet';
 import StepsToUse from '../components/StepsToUse/StepsToUse';
 import WhyUse from '../components/WhyUse/WhyUse';
+import data from '../components/FleetSlider/FleeSlider.data';
 
 function Home() {
-  const [cars, setCars] = useState([]);
-
-  useEffect(() => {
-    fetchCars()
-      .then((vehicles) => {
-        setCars(vehicles);
-      });
-  }, []);
 
   return (
     <div className="page">
       <HeroHome />
       <WhyUse />
       <StepsToUse />
-      <FleetSlider data={cars} />
+      <FleetSlider data={data.cars} />
       <HowToGet />
     </div>
   );
