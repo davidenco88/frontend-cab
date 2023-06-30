d3bf5baa908704d8989b2ee3b177bd49af261761
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-06-30 14:45:01 -0500

    feat: aprproach fliters

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,11 @@
 import UserProfile from "../components/Profile/UserProfile/UserProfile";
-
+import Example from "../components/FilteredTable/FilteredTable";
 function Profile() {
   return (
     <div className="page">
       <h1>USER PAGE UNDER CONSTRUCTION</h1>
       <UserProfile />
+      <Example />
     </div>
   );
 }
