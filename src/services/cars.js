d1866c99d47cd906ee4e8e2bef06787868ace6d6
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-07-05 12:05:51 -0500

    feat: :sparkles: fetch-AvailableVehiclesWorking (#35)

--- src/services/cars.js
+++ src/services/cars.js
@@ -1,8 +1,8 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:8080';
 // const carsUrl = 'http://localhost:3000/cars';
 
-export async function fetchCars() {
-  const res = await fetch(`${BASE_URL}/api/cars`);
+export async function fetchAvailableCars() {
+  const res = await fetch(`${BASE_URL}/api/vehicles/availables`);
   return res.json();
 }
 
