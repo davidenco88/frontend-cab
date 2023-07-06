1749c81a35e8fd2a17c90d2d69914c42d395f798
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-07-06 17:10:31 -0500

    feat: TABLA DE VIAJES AGENDADOS

--- src/components/Trips/TripsScheduled/TripsScheduled.jsx
+++ src/components/Trips/TripsScheduled/TripsScheduled.jsx
@@ -1,21 +1,100 @@
-import "./TripsScheduled.scss";
 
-function UserProfile() {
-  return (
-    <div className="userProfile">
-      {/* <div className="userProfile__pic"> */}
-      <img
-        className="userProfile__img"
-        src="https://picsum.photos/400"
-        alt="random photo"
-      />
-      {/* </div> */}
-      <div className="userProfile__text">
-        <h1 className="userProfile__text__name">Federico Cordoba</h1>
-        <h2 className="userProfile__text__email">email@example.com</h2>
-      </div>
-    </div>
+import React, { useMemo } from 'react';
+import { MaterialReactTable } from 'material-react-table';
+
+//nested data is ok, see accessorKeys in ColumnDef below
+const data = [
+  {
+    name: {
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+    address: '261 Erdman Ford',
+    city: 'East Daphne',
+    state: 'Kentucky',
+    date: "2023/08/07"
+  },
+  {
+    name: {
+      firstName: 'Jane',
+      lastName: 'Doe',
+    },
+    address: '769 Dominic Grove',
+    city: 'Columbus',
+    state: 'Ohio',
+    date: "2023/08/09"
+  },
+  {
+    name: {
+      firstName: 'Joe',
+      lastName: 'Doe',
+    },
+    address: '566 Brakus Inlet',
+    city: 'South Linda',
+    state: 'West Virginia',
+    date: "2023/08/17"
+  },
+  {
+    name: {
+      firstName: 'Kevin',
+      lastName: 'Vandy',
+    },
+    address: '722 Emie Stream',
+    city: 'Lincoln',
+    state: 'Nebraska',
+    date: "2023/08/27"
+  },
+  {
+    name: {
+      firstName: 'Joshua',
+      lastName: 'Rolluffs',
+    },
+    address: '32188 Larkin Turnpike',
+    city: 'Charleston',
+    state: 'South Carolina',
+    date: "2023/08/10"
+  },
+];
+
+const TripsTable = () => {
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name.firstName', //access nested data with dot notation
+        header: 'First Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'name.lastName',
+        header: 'Last Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'address', //normal accessorKey
+        header: 'Address',
+        size: 200,
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+        size: 150,
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+        size: 150,
+      },
+      {
+        accessorKey: 'date',
+        header: 'Pickup Date',
+        size: 150,
+      },
+    ],
+    [],
   );
-}
 
-export default UserProfile;
+  return <MaterialReactTable columns={columns} data={data} />;
+};
+
+export default TripsTable;
