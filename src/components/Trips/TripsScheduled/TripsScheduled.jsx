efaee4c6f9801981527875abbb129ebd7797eb0e
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-07-06 15:41:41 -0500

    feat: diseño de viajes 1.0

---
+++ src/components/Trips/TripsScheduled/TripsScheduled.jsx
@@ -0,0 +1,21 @@
+import "./TripsScheduled.scss";
+
+function UserProfile() {
+  return (
+    <div className="userProfile">
+      {/* <div className="userProfile__pic"> */}
+      <img
+        className="userProfile__img"
+        src="https://picsum.photos/400"
+        alt="random photo"
+      />
+      {/* </div> */}
+      <div className="userProfile__text">
+        <h1 className="userProfile__text__name">Federico Cordoba</h1>
+        <h2 className="userProfile__text__email">email@example.com</h2>
+      </div>
+    </div>
+  );
+}
+
+export default UserProfile;
---
+++ src/pages/TripsScheduleds.jsx
@@ -0,0 +1,12 @@
+import UserProfile from "../components/Profile/UserProfile/UserProfile";
+
+function Profile() {
+  return (
+    <div className="page">
+      <h1>USER PAGE UNDER CONSTRUCTION</h1>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default Profile;
