33f83a6913af96a609a9e8df87ae60fa3d3a7ae8
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-07-10 08:26:10 -0500

    Feature/cloudinary profile pic (#44)
    * feat: :sparkles: update-avatar-working
    
    * refactor: :recycle: updateAvatar-using-env4Deploy

--- src/components/Profile/UserProfile/UserProfile.jsx
+++ src/components/Profile/UserProfile/UserProfile.jsx
@@ -1,18 +1,46 @@
-import "./UserProfile.scss";
+import { useState } from 'react';
+import { updateAvatar } from '../../../services/upload';
+import './UserProfile.scss';
 
 function UserProfile() {
+  const [avatar, setAvatar] = useState();
+  const [profile, setProfile] = useState(JSON.parse(localStorage.getItem('profile')));
+
+  const handleUpload = (e) => {
+    setAvatar(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append('avatar', avatar);
+
+    try {
+      const updatedProfile = await updateAvatar(profile, formData);
+      localStorage.setItem('profile', JSON.stringify(updatedProfile.data));
+      setProfile(updatedProfile.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="userProfile">
-      {/* <div className="userProfile__pic"> */}
       <img
         className="userProfile__img"
-        src="https://picsum.photos/400"
-        alt="random photo"
+        src={profile.avatar}
+        alt="profile pic"
       />
-      {/* </div> */}
       <div className="userProfile__text">
-        <h1 className="userProfile__text__name">Federico Cordoba</h1>
-        <h2 className="userProfile__text__email">email@example.com</h2>
+        <form action="submit" onSubmit={handleSubmit}>
+          <input type="file" accept="image/*" name="avatar" onChange={handleUpload} />
+          <button type="submit">Upload</button>
+        </form>
+        <h1 className="userProfile__text__name">{profile.fullName}</h1>
+        <h2 className="userProfile__text__email">{profile.email}</h2>
+        <h2 className="userProfile__text__email">{profile.roles[0].name}</h2>
       </div>
     </div>
   );
---
+++ src/services/upload.js
@@ -0,0 +1,18 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:8080';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function updateAvatar(profile, formData) {
+  const options = {
+    method: 'PATCH',
+    body: formData,
+  };
+  try {
+    const res = await fetch(`${BASE_URL}/api/users/avatar/${profile.id}`, options);
+
+    if (res.ok) {
+      return { status: res.status, data: await res.json() };
+    }
+  } catch (error) {
+    throw new Error('Failed to update Avatar', error.message);
+  }
+}
